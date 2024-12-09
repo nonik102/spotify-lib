c1480f5f44ff7cde0bb34bd52ef3e45874141513
Noah McCarthy <nonik102@gmail.com>
2024-12-09 00:52:01 -0500

    actually sanitize

--- spotify_lib/common.py
+++ spotify_lib/common.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
 from typing import NewType
 
-SPOTIFY_CLIENT_ID="2cb8cf45c61b4adba65390250e7b62dd"
-SPOTIFY_SECRET="e7ed2693ef2044fda0c01cbd8464bcce"
 SPOTIFY_SECRET_FILE_NAME="spotify_api"
 TOKEN_PATH="/home/nonik/.tokens"
 
