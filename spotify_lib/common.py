177bd55a7dc0861f6061bdfcad4db78fa211d13d
Noah McCarthy <59943770+nonik102@users.noreply.github.com>
2024-12-23 13:52:21 -0500

    Build base api (#1)
    * add token provider
    
    * sanitize
    
    * actually sanitize
    
    * add tests
    
    * cleanup auth
    
    * add search endpoint
    
    * cleanup
    
    ---------
    
    Co-authored-by: Noah McCarthy <nonik102@gmail.com>

---
+++ spotify_lib/common.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import NewType
+
+SPOTIFY_SECRET_FILE_NAME="spotify_api"
+
+JsonBlob = NewType("JsonBlob", dict)
+SpotifyID = NewType("SpotifyID", str)
+
+class SpotifyItemType(Enum):
+    ALBUM = "album"
+    ARTIST = "artist"
+    PLAYLIST = "playlist"
+    TRACK = "track"
+    SHOW = "show"
+    EPISODE = "episode"
+    AUDIOBOOK = "audiobook"
+
+    def __str__(self) -> str:
+        return self.value
+
+@dataclass
+class Token:
+    value: str
+    type: str
+    alive_seconds: int
+
+    def __str__(self) -> str:
+        return self.value
